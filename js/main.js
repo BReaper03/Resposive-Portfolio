4afc72cb4ec185f9644fcc3f736520f13d6bfd86
BReaer03 <gabrielpaez011@gmail.com>
2021-09-04 02:00:18 -0400

    fALLA 1

--- js/main.js
+++ js/main.js
@@ -4,10 +4,5 @@ window.addEventListener('scroll', function () {
 })
 
 //scrollReveal 
-ScrollReveal().reveal('.banner', {delay:300});
-ScrollReveal().reveal('.about', {delay:300});
-ScrollReveal().reveal('.services', {delay:300});
-ScrollReveal().reveal('.work', {delay:300});
-ScrollReveal().reveal('.testimonial', {delay:300});
-ScrollReveal().reveal('.contact', {delay:300});
+
 
